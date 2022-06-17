c31f6549975873c1476b99e5398a33db13f5fd8d
Wolf <caffeinate@msn.com>
2022-06-17 17:45:22 -0400

    With conversion

--- helpers/fetch.py
+++ helpers/fetch.py
@@ -1,22 +1,47 @@
 import pytube
-import asyncio
 import os
+import subprocess
+import threading
+import asyncio
+from random import randint
+from mutagen.mp3 import MP3
 
-async def fetch(url: str, filepath=None):
+
+async def fetch(url: str) -> int:
     """
     Download audio of youtube video and automaticaly inject metadata
 
     Args:
         url (str): URL of video to download.
         filepath (str, optional): Filepath up to the name of file. File extension automatically added.
-    """
-    stream = pytube.YouTube(url) # fetch youtube video
-    stream = stream.streams.filter(only_audio=True, mime_type="audio/mp4") # fetch all versions of video/audio
-    stream = sorted(stream, key=lambda stream: stream.filesize, reverse=True) # sort by filesize
-    stream = stream[0] # choose largest file
-    stream.download() # download file
-    stream.filetype = stream.mime_type.split("/")[1] # obtain file extension
 
-    # if given a specified filepath then use it
-    if isinstance(filepath, str):
-        os.rename(f"{stream.title}.{stream.filetype}", f"{stream.filepath}.{stream.filetype}")
+    Returns:
+        id of downloaded file. (name of file = "{id}.mp4")
+    """
+
+    id = randint(1000000, 9999999)
+
+    # fetch and download youtube .webm audio for given URL
+    stream = pytube.YouTube(url)
+    stream = stream.streams.filter(only_audio=True, mime_type="audio/webm")
+    stream = sorted(stream, key=lambda stream: stream.filesize, reverse=True)
+    stream = stream[0]
+    stream.download()
+    os.rename(f"{stream.title}.webm", f"{id}.webm")
+
+    # convert audio file
+    def convert():
+        subprocess.run(
+            f'ffmpeg -i "{id}.webm" "{id}.mp3" -hide_banner -loglevel error'
+        )
+        os.remove(f"{id}.webm")
+    
+    conversion = threading.Thread(target=convert).start()
+    while conversion.is_alive():
+        await asyncio.sleep(.2)
+
+    audio = MP3(f"{id}.mp3")
+    print(audio.info.length)
+    print(audio.info.bitrate)
+
+    return id
